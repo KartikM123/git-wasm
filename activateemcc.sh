810b438beee30acf3b1f03adf1fddf3cb83ad234
Kartik Mahajan <kamahajan@microsoft.com>
2022-05-03 01:24:33 +0000

    init

---
+++ activateemcc.sh
@@ -0,0 +1,13 @@
+echo "entering emcc"
+cd emsdk
+# Download and install the latest SDK tools.
+./emsdk install latest
+
+# Make the "latest" SDK "active" for the current user. (writes .emscripten file)
+./emsdk activate latest
+
+# Activate PATH and other environment variables in the current terminal
+source ./emsdk_env.sh
+echo "done installing"
+cd ..
+emcc
